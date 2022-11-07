6fdbaf0cd7d3cd554c1d141b5d8097394cb8ecc5
hdd2k <hank.lee.qed@gmail.com>
2022-11-07 09:40:16 -0800

    Add first codeforces problem solution

---
+++ main.py
@@ -0,0 +1,1 @@
+import watermelon.main as w
---
+++ watermelon/main.py
@@ -0,0 +1,13 @@
+
+# https://codeforces.com/problemset/problem/4/A
+
+
+def solve():
+    w = int(input())
+    if w % 2 == 0 and w != 2:
+        print('YES')
+    else:
+        print('NO')
+
+
+solve()
